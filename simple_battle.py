a9350e57c795651d35433782b0e92365db1afbe0
Jonathan Domingo <jonodomingo@gmail.com>
2020-04-07 22:17:30 -0700

    Another check to see if commit is working from VSCode

--- simple_battle.py
+++ simple_battle.py
@@ -47,7 +47,7 @@ while game_running == True:
 
         # If the monster died, end the game:
         if player['health'] <= 0:
-            pass # Pass allows for no action, to be replaced.
+            pass # Pass allows for no action.
 
         # Show the user monster/player health after attacks.
         print(monster['health'])
